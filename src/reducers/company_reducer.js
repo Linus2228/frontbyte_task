0e2df16d87225db0eb4aa461d9379648cdad895b
Dzmitry Kudla <Linus2228@gmail.com>
2020-03-27 18:38:16 +0300

    refactoring: got rid off mock data

--- src/reducers/company_reducer.js
+++ src/reducers/company_reducer.js
@@ -1,29 +1,7 @@
 import { GET_SUMMARY } from "../actions/types";
 
-const summary = {
-  Trainings: [
-    {
-      Name: "training one",
-      Progress: 56
-    },
-    {
-      Name: "training two",
-      Progress: 88
-    },
-    {
-      Name: "training three",
-      Progress: 56
-    },
-    {
-      Name: "training four",
-      Progress: 12
-    }
-  ],
-  Working: 26
-};
-
 const initialState = {
-  summary,
+  summary: {},
   users: []
 };
 
